da8126d68d3738b9430d03e626a13f3d28c8ebe4
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-11-17 08:56:30 +0200

    Create init.py

---
+++ src/document_processing/init.py
@@ -0,0 +1,11 @@
+from .resume_parser import ResumeParser
+from .ai_rewriter import ResumeRewriter
+from .document_storage import DocumentStorage
+from .compliance_za import ComplianceGeneratorZA
+
+__all__ = [
+    "ResumeParser",
+    "ResumeRewriter", 
+    "DocumentStorage",
+    "ComplianceGeneratorZA"
+]
