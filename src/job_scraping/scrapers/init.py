c43c71bf4932eafda36ec9e273188fe3ba24a504
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2025-11-17 09:00:49 +0200

    Create init.py

---
+++ src/job_scraping/scrapers/init.py
@@ -0,0 +1,13 @@
+from .za_agencies import SouthAfricaJobScrapers
+from .international_za import InternationalJobScraper
+from .careerjunction import CareerJunctionScraper
+from .pnet import PNetScraper
+from .indeed_za import IndeedZAScraper
+
+__all__ = [
+    "SouthAfricaJobScrapers",
+    "InternationalJobScraper",
+    "CareerJunctionScraper", 
+    "PNetScraper",
+    "IndeedZAScraper"
+]
